9553d84c3b405707d9b00e0ac68bce746e57c7e5
LeandroDoudement <leandrodoudement@gmail.com>
2023-03-08 15:20:17 -0300

    Requisito 12 | POST /post2

--- src/controllers/Category.js
+++ src/controllers/Category.js
@@ -3,7 +3,6 @@ const categoryService = require('../services/Category');
 const createCategory = async (req, res) => {
   try {
     const { name } = req.body;
-    console.log(name);
     await categoryService.createCategory({ name });
     const category = await categoryService.getByName(name);
     res.status(201).json(category);
